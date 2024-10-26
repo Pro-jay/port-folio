a1d8fc547c96f8548cf174ce6abea54bdcd86aca
Pro-jay <projaycobest@gmail.com>
2024-10-26 21:01:20 +0100

    first commit

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -175,8 +175,31 @@ const Resume = () => {
           <div className="min-h-[70vh] w-full">
             {/* experience */}
             <TabsContent value={"experience"} className="w-full">
-              experience
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <span>{item.duration}</span>
+                          <h3>{item.position}</h3>
+                          <div>
+                            {/* dot */}
+                            <span></span>
+                            <p>{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
+
             {/* education */}
             <TabsContent value={"education"} className="w-full">
               education
